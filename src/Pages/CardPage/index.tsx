fd3534e03e502d3fda77a0b00043c94baa5251d8
olga <olga.goryszewska@gmail.com>
2024-06-03 14:47:49 +0200

    installing  a router and creating pages

---
+++ src/Pages/CardPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CardPage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page</p>
+    </div>
+  );
+};
+
+export default CardPage;
---
+++ src/Pages/ContactPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Checkout page</h1>
+      <p>Welcome to the Home Page</p>
+    </div>
+  );
+};
+
+export default ContactPage;
