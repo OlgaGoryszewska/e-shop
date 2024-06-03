f8591c8f9845abbb82eec09bf56bc378097de18c
olga <olga.goryszewska@gmail.com>
2024-06-03 10:28:05 +0200

    adding header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import react from "react";
-import Nav from "../Nav";
+import Nav from "../Header";
 import Layout from "../Layout";
 
 function App() {
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import logo_znak_wodny from "../../assets/logo_znak_wodny.png";
 import { useState } from "react";
 import DropDownMenu from "../DropDownMenu";
 
-const Nav = () => {
+const Header = () => {
   return (
     <div className="flex flex-row justify-between max-h-12  ">
       <img className="mt-2 " src={logo_znak_wodny} alt="logo" />
@@ -20,4 +20,4 @@ const Nav = () => {
   );
 };
 
-export default Nav;
+export default Header;
