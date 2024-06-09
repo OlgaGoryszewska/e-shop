5748db0b1dd87c11766e3276b36443f4fd3062e2
olga <olga.goryszewska@gmail.com>
2024-06-09 23:17:22 +0200

    styling pages

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,11 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 
 export default function Layout() {
   return (
-    <div className="">
+    <div className="flex flex-col">
       <Header />
       <main className="flex-grow">
         <Outlet />
